39a230012e8fc2b88c93596cf8cce2d8e47255e2
Mikhail Tuchkov <mtuchkov@microsoft.com>
2016-04-14 21:46:26 -0700

    delegating pipeline

---
+++ csharp/device/Microsoft.Azure.Devices.Client/DeviceClientDelegatingHandler.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Azure.Devices.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client.Common;
+
+    abstract class DeviceClientDelegatingHandler : IDelegatingHandler
+    {
+        static readonly Task<Message> DummyResultObject = Task.FromResult((Message)null);
+
+        protected volatile IDelegatingHandler innerHandler;
+
+        public IDelegatingHandler InnerHandler
+        {
+            get { return this.innerHandler; }
+            set { this.innerHandler = value; }
+        }
+
+        protected DeviceClientDelegatingHandler()
+            : this(null)
+        {
+        }
+
+        protected DeviceClientDelegatingHandler(DeviceClientDelegatingHandler innerHandler)
+        {
+            this.InnerHandler = innerHandler;
+        }
+
+        public virtual Task OpenAsync(bool explicitOpen)
+        {
+            return this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler.OpenAsync(explicitOpen);
+        }
+
+        public virtual async Task CloseAsync()
+        {
+            await (this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler.CloseAsync()).ContinueWith(t => GC.SuppressFinalize(this), TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+
+        public virtual Task<Message> ReceiveAsync()
+        {
+            return this.InnerHandler == null ? DummyResultObject : this.InnerHandler.ReceiveAsync();
+        }
+
+        public virtual Task<Message> ReceiveAsync(TimeSpan timeout)
+        {
+            return this.InnerHandler == null ? DummyResultObject : this.InnerHandler.ReceiveAsync(timeout);
+        }
+
+        public virtual Task CompleteAsync(string lockToken)
+        {
+            return this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler.CompleteAsync(lockToken);
+        }
+
+        public virtual Task AbandonAsync(string lockToken)
+        {
+            return this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler.AbandonAsync(lockToken);
+        }
+
+        public virtual Task RejectAsync(string lockToken)
+        {
+            return this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler.RejectAsync(lockToken);
+        }
+
+        public virtual Task SendEventAsync(Message message)
+        {
+            return this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler.SendEventAsync(message);
+        }
+
+        public virtual Task SendEventAsync(IEnumerable<Message> messages)
+        {
+            return this.InnerHandler == null ? TaskConstants.Completed : this.InnerHandler.SendEventAsync(messages);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);   
+            GC.SuppressFinalize(this);
+        }
+
+        ~DeviceClientDelegatingHandler()
+        {
+            this.Dispose(false);
+        }
+    }
+}
---
+++ csharp/device/Microsoft.Azure.Devices.Client/Transport/RoutingHandler.cs
@@ -0,0 +1,82 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Azure.Devices.Client.Transport
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client.Exceptions;
+
+    class RoutingHandler : DeviceClientDelegatingHandler
+    {
+        internal delegate DeviceClientDelegatingHandler TransportHandlerFactory(IotHubConnectionString iotHubConnectionString, ITransportSettings transportSettings);
+
+        readonly TransportHandlerFactory transportHandlerFactory;
+        readonly IotHubConnectionString iotHubConnectionString;
+        readonly ITransportSettings[] transportSettings;
+
+        public RoutingHandler(TransportHandlerFactory transportHandlerFactory, IotHubConnectionString iotHubConnectionString, ITransportSettings[] transportSettings)
+        {
+            this.transportHandlerFactory = transportHandlerFactory;
+            this.iotHubConnectionString = iotHubConnectionString;
+            this.transportSettings = transportSettings;
+        }
+
+        public override async Task OpenAsync(bool explicitOpen)
+        {
+            await this.TryOpenPrioritizedTransportsAsync(explicitOpen);
+        }
+
+        async Task TryOpenPrioritizedTransportsAsync(bool explicitOpen)
+        {
+            Exception lastException = null;
+            // Concrete Device Client creation was deferred. Use prioritized list of transports.
+            foreach (ITransportSettings transportSetting in this.transportSettings)
+            {
+                try
+                {
+                    this.InnerHandler = this.transportHandlerFactory(this.iotHubConnectionString, transportSetting);
+                    
+                    // Try to open a connection with this transport
+                    await this.InnerHandler.OpenAsync(explicitOpen);
+                }
+                catch (Exception exception)
+                {
+                    if (this.InnerHandler != null)
+                    {
+                        await this.InnerHandler.CloseAsync();
+                    }
+                    if (!(exception is IotHubCommunicationException || exception is TimeoutException || exception is SocketException || exception is AggregateException))
+                    {
+                        throw;
+                    }
+
+                    var aggregateException = exception as AggregateException;
+                    if (aggregateException != null)
+                    {
+                        ReadOnlyCollection<Exception> innerExceptions = aggregateException.Flatten().InnerExceptions;
+                        if (!innerExceptions.Any(x => x is IotHubCommunicationException || x is SocketException || x is TimeoutException))
+                        {
+                            throw;
+                        }
+                    }
+
+                    lastException = exception;
+
+                    // open connection failed. Move to next transport type
+                    continue;
+                }
+
+                return;
+            }
+
+            if (lastException != null)
+            {
+                throw new InvalidOperationException("Unhandled exception", lastException);
+            }
+        }
+    }
+}
